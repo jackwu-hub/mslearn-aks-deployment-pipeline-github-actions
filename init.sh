3cab0837fdc992e3b1ee47d3e6de62664958cf3f
jackwu-hub <542910960@qq.com>
2022-09-30 14:58:12 +0000

    执行init.sh

--- init.sh
+++ init.sh
@@ -7,8 +7,8 @@ echo "Obtaining credentials..."
 az aks get-credentials -n $AKS_NAME -g $RESOURCE_GROUP_NAME
 
 export DNS_NAME=$(az network dns zone list -o json --query "[?contains(resourceGroup,'$RESOURCE_GROUP_NAME')].name" -o tsv)
-sed -i '' 's+!IMAGE!+'"$ACR_NAME"'/contoso-website+g' kubernetes/deployment.yaml
-sed -i '' 's+!DNS!+'"$DNS_NAME"'+g' kubernetes/ingress.yaml
+sed -i  's+!IMAGE!+'"$ACR_NAME"'/contoso-website+g' kubernetes/deployment.yaml
+sed -i  's+!DNS!+'"$DNS_NAME"'+g' kubernetes/ingress.yaml
 echo "Installation concluded, copy these values and store them, you'll use them later in this exercise:"
 echo "-> Resource Group Name: $RESOURCE_GROUP_NAME"
 echo "-> ACR Name: $ACR_NAME"
